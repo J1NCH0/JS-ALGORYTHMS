75bb0014ad8a73a534270d2f3acc28c7b9bd3772
DavitJincharadze <113067688+J1NCH0@users.noreply.github.com>
2023-04-03 19:08:23 +0400

    delivery ready

---
+++ Delivery/script.js
@@ -0,0 +1,7 @@
+let shippingIsPossible = false;
+
+if (shippingIsPossible) {
+    console.log('თქვენს მისამართზე მიტანის სერვისი ხელმისაწვდომი არის')
+} else  {
+    console.log('თქვენს მისამართზე მიტანის სერვისი ხელმისაწვდომი არ არის')
+}
